5e93109df81bceccb1b36f970dca9ac7106af305
Thomas Mechelke <tomjmech@gmail.com>
2023-04-20 15:16:04 +0200

    Add Punctuator to Token

--- src/bootstrap-rs/src/eureka/token.rs
+++ src/bootstrap-rs/src/eureka/token.rs
@@ -1,14 +1,17 @@
 use identifier::Identifier;
 use keyword::Keyword;
 use padding::Padding;
+use punctuator::Punctuator;
 
 mod identifier;
 mod keyword;
 mod name;
 mod padding;
+mod punctuator;
 
 enum Token<'a> {
     Identifier(Identifier<'a>),
     Keyword(Keyword),
     Padding(Padding<'a>),
+    Punctuator(Punctuator),
 }
---
+++ src/bootstrap-rs/src/eureka/token/punctuator.rs
@@ -0,0 +1,50 @@
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub enum Punctuator {
+    LeftParenthesis,
+    RightParenthesis,
+    LeftBrace,
+    RightBrace,
+}
+
+impl Punctuator {
+    pub fn lex(src: &str) -> Option<(Punctuator, &str)> {
+        let mut chars = src.chars();
+
+        let punctuator = match chars.next() {
+            Some('(') => Punctuator::LeftParenthesis,
+            Some(')') => Punctuator::RightParenthesis,
+            Some('{') => Punctuator::LeftBrace,
+            Some('}') => Punctuator::RightBrace,
+            _ => return None,
+        };
+
+        Some((punctuator, chars.as_str()))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn lex_succeeds() {
+        for (src, expected_punctuator, expected_remaining_src) in [
+            ("(a", Punctuator::LeftParenthesis, "a"),
+            (")", Punctuator::RightParenthesis, ""),
+            ("{\n    ", Punctuator::LeftBrace, "\n    "),
+            ("} else {\r\n", Punctuator::RightBrace, " else {\r\n"),
+        ] {
+            let (actual_punctuator, actual_remaining_src) = Punctuator::lex(src).unwrap();
+
+            assert_eq!(expected_punctuator, actual_punctuator);
+            assert_eq!(expected_remaining_src, actual_remaining_src);
+        }
+    }
+
+    #[test]
+    fn lex_fails() {
+        for src in ["", "x", "1", "if", " ", "#"] {
+            assert!(Punctuator::lex(src).is_none());
+        }
+    }
+}
