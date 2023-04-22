acf4b87297f218c16c2fdd6b4fa107b29fe56064
Thomas Mechelke <tomjmech@gmail.com>
2023-04-22 12:00:38 +0200

    Implement lex on Token

--- src/bootstrap-rs/src/eureka/token.rs
+++ src/bootstrap-rs/src/eureka/token.rs
@@ -19,6 +19,28 @@ enum Token {
     Punctuator(Punctuator),
 }
 
+impl Token {
+    pub fn lex(src: &str) -> Option<(Token, &str)> {
+        if let Some((identifier, remaining_src)) = Identifier::lex(src) {
+            return Some((identifier.into(), remaining_src));
+        }
+
+        if let Some((keyword, remaining_src)) = Keyword::lex(src) {
+            return Some((keyword.into(), remaining_src));
+        }
+
+        if let Some((padding, remaining_src)) = Padding::lex(src) {
+            return Some((padding.into(), remaining_src));
+        }
+
+        if let Some((punctuator, remaining_src)) = Punctuator::lex(src) {
+            return Some((punctuator.into(), remaining_src));
+        }
+
+        None
+    }
+}
+
 impl From<Identifier> for Token {
     fn from(value: Identifier) -> Token {
         Token::Identifier(value)
@@ -46,5 +68,48 @@ impl From<Padding> for Token {
 impl From<Punctuator> for Token {
     fn from(value: Punctuator) -> Token {
         Token::Punctuator(value)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn lex_empty_main() {
+        let src = "fn main() {}";
+
+        let (token, src) = Token::lex(src).unwrap();
+        assert_eq!(token, Keyword::Fn.into());
+
+        let (token, src) = Token::lex(src).unwrap();
+        assert_eq!(token, Padding::new(" ").into());
+
+        let (token, src) = Token::lex(src).unwrap();
+        assert_eq!(token, UnquotedIdentifier::new("main").into());
+
+        let (token, src) = Token::lex(src).unwrap();
+        assert_eq!(token, Punctuator::LeftParenthesis.into());
+
+        let (token, src) = Token::lex(src).unwrap();
+        assert_eq!(token, Punctuator::RightParenthesis.into());
+
+        let (token, src) = Token::lex(src).unwrap();
+        assert_eq!(token, Padding::new(" ").into());
+
+        let (token, src) = Token::lex(src).unwrap();
+        assert_eq!(token, Punctuator::LeftBrace.into());
+
+        let (token, src) = Token::lex(src).unwrap();
+        assert_eq!(token, Punctuator::RightBrace.into());
+
+        assert!(src.is_empty());
+    }
+
+    #[test]
+    fn lex_fails() {
+        for src in ["", "\0", "\x1B"] {
+            assert!(Token::lex(src).is_none());
+        }
     }
 }
