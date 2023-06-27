2e58dfc869a07c64a3dba8a8da277dd395a7067d
Thomas Mechelke <tomjmech@gmail.com>
2023-06-27 17:56:48 +0200

    Generalize the parse functions

--- src/bootstrap-rs/src/eureka/syntax_tree/parse.rs
+++ src/bootstrap-rs/src/eureka/syntax_tree/parse.rs
@@ -1,22 +1,25 @@
-use crate::eureka::token::{Padding, Token, Tokens};
+use crate::eureka::token::{Token, Tokens};
+use crate::miscellaneous::DisplayName;
 
-pub fn optional_padding(tokens: &mut Tokens) -> Option<Padding> {
-    if let Some(Token::Padding(padding)) = tokens.peek().cloned() {
-        tokens.pop();
-        Some(padding)
-    } else {
-        None
+pub fn optional<T: TryFrom<Token>>(tokens: &mut Tokens) -> Option<T> {
+    if let Some(token) = tokens.peek().cloned() {
+        if let Ok(t) = T::try_from(token) {
+            tokens.pop();
+            return Some(t);
+        }
     }
+
+    None
 }
 
-pub fn padding(tokens: &mut Tokens) -> Result<Padding, String> {
-    optional_padding(tokens).ok_or_else(|| "missing padding".to_string())
+pub fn required<T: DisplayName + TryFrom<Token>>(tokens: &mut Tokens) -> Result<T, String> {
+    optional::<T>(tokens).ok_or_else(|| format!("missing {}", T::display_name()))
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::eureka::token::Keyword;
+    use crate::eureka::token::{Keyword, Padding};
 
     #[test]
     fn test_optional_padding() {
@@ -24,29 +27,29 @@ mod tests {
 
         assert_eq!(tokens.peek(), Some(&Token::Padding(Padding::new(" "))));
 
-        let padding1 = optional_padding(&mut tokens);
+        let padding1 = optional::<Padding>(&mut tokens);
 
         assert_eq!(padding1, Some(Padding::new(" ")));
         assert_eq!(tokens.peek(), Some(&Token::Keyword(Keyword::Fn)));
 
-        let padding2 = optional_padding(&mut tokens);
+        let padding2 = optional::<Padding>(&mut tokens);
 
         assert_eq!(padding2, None);
         assert_eq!(tokens.peek(), Some(&Token::Keyword(Keyword::Fn)));
     }
 
     #[test]
-    fn test_padding() {
+    fn test_required_padding() {
         let mut tokens = Tokens::lex_all(" fn").unwrap();
 
         assert_eq!(tokens.peek(), Some(&Token::Padding(Padding::new(" "))));
 
-        let padding1 = padding(&mut tokens);
+        let padding1 = required::<Padding>(&mut tokens);
 
         assert_eq!(padding1, Ok(Padding::new(" ")));
         assert_eq!(tokens.peek(), Some(&Token::Keyword(Keyword::Fn)));
 
-        let padding2 = padding(&mut tokens);
+        let padding2 = required::<Padding>(&mut tokens);
 
         assert_eq!(padding2, Err("missing padding".to_string()));
         assert_eq!(tokens.peek(), Some(&Token::Keyword(Keyword::Fn)));
