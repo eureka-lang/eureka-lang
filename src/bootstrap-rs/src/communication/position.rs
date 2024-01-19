b997d568dee89c0463d3308e07ae77ff50980bfc
Thomas Mechelke <tomjmech@gmail.com>
2024-01-19 17:29:31 +0100

    Improve test in position.rs

--- src/bootstrap-rs/src/communication/position.rs
+++ src/bootstrap-rs/src/communication/position.rs
@@ -68,19 +68,19 @@ mod tests {
 
     #[test]
     fn mut_position() {
-        let mut position = Position::new(2, 1);
+        let mut position = Position::new(2, 9);
 
         assert_eq!(position.line(), 2);
-        assert_eq!(position.column(), 1);
+        assert_eq!(position.column(), 9);
 
-        position.set_line(8);
+        position.set_line(5);
 
-        assert_eq!(position.line(), 8);
-        assert_eq!(position.column(), 1);
+        assert_eq!(position.line(), 5);
+        assert_eq!(position.column(), 9);
 
         position.set_column(4);
 
-        assert_eq!(position.line(), 8);
+        assert_eq!(position.line(), 5);
         assert_eq!(position.column(), 4);
     }
 
