1f80cbdc0645d355932fe3883b02e6f2d047eb27
Thomas Mechelke <tomjmech@gmail.com>
2024-02-23 08:37:31 +0100

    Move wasm::Module to wasm::module::Module

--- src/bootstrap-rs/src/wasm.rs
+++ src/bootstrap-rs/src/wasm.rs
@@ -11,46 +11,11 @@ pub use instruction_sequence::InstructionSequence;
 mod instruction_sequence;
 
 mod leb128;
+
+pub use module::Module;
+mod module;
+
 mod types;
 
 pub use vector::Vector;
 mod vector;
-
-#[derive(Clone, Debug, Eq, Hash, PartialEq)]
-pub struct Module;
-
-impl Module {
-    const MAGIC_NUMBER: [u8; 4] = [0x00, 0x61, 0x73, 0x6D];
-    const VERSION: [u8; 4] = [0x01, 0x00, 0x00, 0x00];
-
-    pub fn new() -> Module {
-        Module
-    }
-
-    pub fn encode(&self) -> Vec<u8> {
-        let mut buffer = Vec::new();
-
-        buffer.extend(Self::MAGIC_NUMBER);
-        buffer.extend(Self::VERSION);
-
-        buffer
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn encode_empty() {
-        let module = Module::new();
-
-        let actual = module.encode();
-        let expected = vec![
-            0x00, 0x61, 0x73, 0x6D, // magic number
-            0x01, 0x00, 0x00, 0x00, // version
-        ];
-
-        assert_eq!(expected, actual);
-    }
-}
